d6691552b0f75f2fe1237a156d828034b2b9d26d
RuslanBoyarkin <boyarkin.rich@gmail.com>
2022-02-18 16:04:22 +0600

    updated homeWork

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -4,7 +4,9 @@ public class MyFirstClass {
 
     public static void main(String[] args) {
         MySecondClass secondClassInstance = new MySecondClass();
-        System.out.println(secondClassInstance.homeWork());
+        System.out.println(secondClassInstance.homeWork(3));
+        System.out.println(secondClassInstance.returnString("homeWork"));
+
     }
 }
 
--- src/main/java/MySecondClass.java
+++ src/main/java/MySecondClass.java
@@ -1,5 +1,8 @@
 public class MySecondClass {
-    public String homeWork(){
+    public String homeWork(int i){
         return "I did my Home Work!";
+    }
+    public static String returnString(String s) {
+        return "~~~<тут то, что вернул метод homeWork>~~~";
     }
 }
